8b8305b05967b4ea887232335335239ad559f448
sampathkumar040 <sampathkumar040@gmail.com>
2022-11-25 20:01:47 +0530

    stash commanf demo file

--- Demo.java
+++ Demo.java
@@ -1,9 +1,20 @@
 hi all
 good morning
 i am stay in Hyderabad 
+<<<<<<< Updated upstream
 class demo{
 void m2(){
 System.out.println("Hi sampath");
 
 }
 }
+=======
+
+class Demo{
+void m1(){
+System.out.println("Hello");
+
+}
+
+}
+>>>>>>> Stashed changes
